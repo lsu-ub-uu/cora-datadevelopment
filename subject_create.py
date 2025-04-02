aa2a839d212858f1396c136face3383fb7bfd866
Sara Tobiasson <sara.tobiasson@ub.uu.se>
2025-04-02 17:09:46 +0200

    added relationLinks

--- subject_create.py
+++ subject_create.py
@@ -1,8 +1,8 @@
-from multiprocessing import Pool
-import time
 import requests
 import xml.etree.ElementTree as ET
+import time
 from collections import OrderedDict 
+from multiprocessing import Pool
 from commondata import CommonData
 from constantsdata import ConstantsData
 from secretdata import SecretData
@@ -20,21 +20,20 @@ def start():
     list_dataRecord = []
     for data_record in dataList.findall('.//DATA_RECORD'):
         list_dataRecord.append(data_record)
-        # createdCoraRecord = create_record(data_record)
-        # relationOldNewIds, linksToEarlierIds, linksToBroaderIds = store_ids(data_record, createdCoraRecord)
+        createdCoraRecord = create_record(data_record)
+        relationOldNewIds, linksToEarlierIds, linksToBroaderIds = store_ids(data_record, createdCoraRecord)
 
-        # print(f"relation: {relationOldNewIds}")
-        # print(f"pre: {linksToEarlierIds}")
-        # print(f"host: {linksToBroaderIds}")
+        print(f"relation: {relationOldNewIds}")
+        print(f"earlier: {linksToEarlierIds}")
+        print(f"broader: {linksToBroaderIds}")
 
-    # loop_id_lists(relationOldNewIds, linksToEarlierIds, linksToBroaderIds)
+    loop_id_lists(relationOldNewIds, linksToEarlierIds, linksToBroaderIds)
 
-    if __name__ == "__main__":
+    """ if __name__ == "__main__":
         with Pool(WORKERS) as pool:
-            pool.map(validate_record, list_dataRecord)
+            pool.map(validate_record, list_dataRecord) """
 
     print(f'Tidsåtgång: {time.time() - starttime}')
-
 
 def new_record_build(data_record):
         newRecordElement = ET.Element(recordType)
@@ -68,6 +67,7 @@ def create_record(data_record):
     if response.status_code not in (200, 201, 409):
         with open('errorlog.txt', 'a', encoding='utf-8') as log:
             log.write(f'{response.status_code}. {response.text}\n\n')
+    return response.text
 
 relationOldNewIds = OrderedDict()
 linksToEarlierIds = OrderedDict()  
@@ -88,6 +88,50 @@ def store_ids(data_record, createdCoraRecord):
         linksToBroaderIds[createdId.text] = broaderIds.text
     return relationOldNewIds, linksToEarlierIds, linksToBroaderIds
 
+def loop_id_lists(relationOldNewIds, linksToEarlierIds, linksToBroaderIds):
+    for oldId, newId in relationOldNewIds.items():
+        if newId in linksToEarlierIds or newId in linksToBroaderIds:
+            repeatId = 0
+            newRecord = read_record_as_xml(newId)
+            cleanedRecord = CommonData.remove_actionLinks_from_record(newRecord, recordType)
+            if newId in linksToBroaderIds:
+                broaderOldId = linksToBroaderIds[newId]
+                broaderNewId = relationOldNewIds[broaderOldId]
+                related_record_build(recordType, cleanedRecord, 'broader', repeatId, broaderNewId)
+                repeatId +=1
+            if newId in linksToEarlierIds:
+                earlierOldIds = linksToEarlierIds[newId]
+                for earlierOldId in earlierOldIds: 
+                    earlierNewId = relationOldNewIds[earlierOldId]
+                    related_record_build(recordType, cleanedRecord, 'earlier', repeatId, earlierNewId)
+                    repeatId +=1
+            update_record(newId, cleanedRecord)
+            print()
+            
+def read_record_as_xml(id):
+    authToken = SecretData.get_authToken(system)
+    headersXml = {'Content-Type':'application/vnd.uub.record+xml', 'Accept':'application/vnd.uub.record+xml', 'authToken':authToken}
+    getRecordUrl = ConstantsData.BASE_URL[system]+"diva-"+recordType+"/"+id
+    response = requests.get(getRecordUrl, headers=headersXml)
+    return ET.fromstring(response.text)
+
+def related_record_build(recordType, cleanedRecord, relatedType, counter, value): 
+    related = ET.SubElement(cleanedRecord, 'related', repeatId=str(counter), type = relatedType)
+    related_recordType = ET.SubElement(related, 'topic')
+    ET.SubElement(related_recordType, 'linkedRecordType').text = "diva-"+recordType
+    ET.SubElement(related_recordType, 'linkedRecordId').text = value
+
+def update_record(id, recordToUpdate):
+    authToken = SecretData.get_authToken(system)
+    headersXml = {'Content-Type':'application/vnd.uub.record+xml', 'Accept':'application/vnd.uub.record+xml', 'authToken':authToken}
+    recordUrl = ConstantsData.BASE_URL[system]+"diva-"+recordType+"/"+id
+    output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+ET.tostring(recordToUpdate).decode("UTF-8")
+    response = requests.post(recordUrl, data=output, headers = headersXml)
+    print(response.status_code, response.text)
+    if response.status_code not in (200, 201, 409):
+        with open('errorlog.txt', 'a', encoding='utf-8') as log:
+            log.write(f'{id}. {response.status_code}. {response.text}\n\n')
+    return response.text
 
 
 start()
