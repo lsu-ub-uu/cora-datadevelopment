376a980eebdd12ebbd43409c95af50d9378f1582
Sara Tobiasson <sara.tobiasson@ub.uu.se>
2025-05-07 13:35:22 +0200

    add logger and threadPool

--- publisher_create.py
+++ publisher_create.py
@@ -1,61 +1,121 @@
-import requests
-import xml.etree.ElementTree as ET
 from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
+import os
+import sys
+import threading
 import time
+sys.path.append(os.path.abspath('src'))
+
+import requests
+from common.RunRotatingLogger import RunRotatingLogger
+
 from commondata import CommonData
 from constantsdata import ConstantsData
-from secretdata import SecretData
+from cora.client.AppTokenClient import AppTokenClient
+from tqdm import tqdm
+import xml.etree.ElementTree as ET
 
 system = 'preview'
-recordType = 'publisher'
+recordType = 'diva-publisher'
+nameInData = 'publisher'
 WORKERS = 16
 filePath_validateBase = (r"validationOrder_base.xml")
-filePath_sourceXml = (r"db_xml\db_diva-"+recordType+".xml")
+filePath_sourceXml = (r"db_xml/publisher_from_db.xml")
+
+request_counter = 0
+app_token_client = None
+data_logger = None
+
 
 def start():
+    global data_logger
+    data_logger = RunRotatingLogger("data", "logs/data_processing.txt").get()
+    data_logger.info("Data processing started")
+    
     starttime = time.time()
+    start_app_token_client()
+    
     dataList = CommonData.read_source_xml(filePath_sourceXml)
     list_dataRecord = []
     for data_record in dataList.findall('.//DATA_RECORD'):
         list_dataRecord.append(data_record)
+    print(f'Number of records read: {len(list_dataRecord)}')
 
-    if __name__ == "__main__":
-        with Pool(WORKERS) as pool:
-            # pool.map(validate_record, list_dataRecord)
-            # pool.map(create_record, list_dataRecord)
-            
-    print(f'Tidsåtgång: {time.time() - starttime}')
+#    with Pool(WORKERS) as pool:
+    with ThreadPool(WORKERS) as pool:
+#    validate
+#        list(tqdm(
+#            pool.imap_unordered(validate_record, list_dataRecord),
+#            total=len(list_dataRecord),
+#            desc="Validating records"
+#        ))
+#    create
+        list(tqdm(
+            pool.imap_unordered(create_record, list_dataRecord),
+            total=len(list_dataRecord),
+            desc="Created records"
+        ))
+    print(f"Tidsåtgång: {time.time() - starttime}")
 
+def start_app_token_client():
+    global app_token_client
+    dependencies = {"requests": requests,
+                    "time": time,
+                    "threading": threading}
+    app_token_client = AppTokenClient(dependencies)
+
+    login_spec = {"login_url": ConstantsData.LOGIN_URLS[system],
+            "login_id": 'divaAdmin@cora.epc.ub.uu.se',
+            "app_token": "49ce00fb-68b5-4089-a5f7-1c225d3cf156"}
+    app_token_client.login(login_spec)
+    
 
 def new_record_build(data_record):
-        newRecordElement = ET.Element(recordType)
-        CommonData.recordInfo_build(recordType, data_record, newRecordElement)
+        newRecordElement = ET.Element(nameInData)
+        CommonData.recordInfo_build(nameInData, data_record, newRecordElement)
         CommonData.name_build(data_record, newRecordElement)
         return newRecordElement
 
+
+
+
+
+
 def validate_record(data_record):
-    authToken = SecretData.get_authToken(system)
-    validate_headers_xml = {'Content-Type':'application/vnd.cora.workorder+xml', 'Accept':'application/vnd.cora.record+xml','authToken':authToken}
-    validate_url = 'https://cora.epc.ub.uu.se/diva/rest/record/workOrder'
+    global app_token_client
+    global data_logger
+    
+    auth_token = app_token_client.get_auth_token()
+    validate_headers_xml = {'Content-Type':'application/vnd.cora.workorder+xml',
+                            'Accept':'application/vnd.cora.record+xml','authToken':auth_token}
+    validate_url = ConstantsData.BASE_URL[system] + 'workOrder'
     newRecordToCreate = new_record_build(data_record)
-    newRecordToValidate = CommonData.validateRecord_build(recordType, filePath_validateBase, newRecordToCreate)
+    newRecordToValidate = CommonData.validateRecord_build(nameInData, filePath_validateBase, newRecordToCreate)
     output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+ET.tostring(newRecordToValidate).decode("UTF-8")
     response = requests.post(validate_url, data=output, headers = validate_headers_xml)
-    print(response.status_code, response.text)
+    oldId_fromSource = CommonData.get_oldId(data_record)
     if '<valid>true</valid>' not in response.text:
-        with open(f'errorlog.txt', 'a', encoding='utf-8') as log:
-            log.write(f"{response.status_code}. {response.text}\n\n")
+        data_logger.error(f"{oldId_fromSource}: {response.status_code}. {response.text}")
+    if response.text:
+        data_logger.info(f"{oldId_fromSource}: {response.status_code}. {response.text}")
+
 
 def create_record(data_record):
-    authToken = SecretData.get_authToken(system)
-    headersXml = {'Content-Type':'application/vnd.cora.record+xml', 'Accept':'application/vnd.cora.record+xml', 'authToken':authToken}
-    urlCreate = ConstantsData.BASE_URL[system]+"diva-"+recordType
+    global app_token_client
+    global data_logger
+    
+    auth_token = app_token_client.get_auth_token()
+    headersXml = {'Content-Type':'application/vnd.cora.recordgroup+xml',
+                  'Accept':'application/vnd.cora.record+xml', 'authToken':auth_token}
+    urlCreate = ConstantsData.BASE_URL[system] + recordType
     recordToCreate = new_record_build(data_record)
     output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+ET.tostring(recordToCreate).decode("UTF-8")
     response = requests.post(urlCreate, data=output, headers = headersXml)
-    print(response.status_code, response.text)
-    if response.status_code not in (200, 201, 409):
-        with open('errorlog.txt', 'a', encoding='utf-8') as log:
-            log.write(f'{response.status_code}. {response.text}\n\n')
+    oldId_fromSource = CommonData.get_oldId(data_record)
+    if response.text:
+        data_logger.info(f"{oldId_fromSource}: {response.status_code}. {response.text}")
+    if response.status_code not in ([201]):
+        data_logger.error(f"{oldId_fromSource}: {response.status_code}. {response.text}")
+    return response.text
 
 start()
