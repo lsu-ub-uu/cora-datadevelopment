794420c83345c859060c83297e5923d9f320490b
Sara Tobiasson <sara.tobiasson@ub.uu.se>
2025-05-13 15:41:32 +0200

    minor name changes

--- commondata.py
+++ commondata.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from abc import abstractstaticmethod
 class  CommonData:
 
     @staticmethod
@@ -13,8 +14,10 @@ class  CommonData:
             element.remove(actionLinks)
 
     @staticmethod
-    def remove_actionLinks_from_record(record, recordType):
-        for clean_record in record.findall(f'.//{recordType}'):
+    def remove_actionLinks_from_record(record, nameInData):
+        print(nameInData)
+        print(ET.dump(record))
+        for clean_record in record.findall(f'.//{nameInData}'):
             for validationType in clean_record.findall('.//validationType'):
                 CommonData.remove_action_link(validationType)
             for dataDivider in clean_record.findall('.//dataDivider'):
@@ -40,7 +43,7 @@ class  CommonData:
         return validationOrder_root
     
     @staticmethod
-    def recordInfo_build(recordType, permission_unit, data_record, newRecordElement):
+    def record_info_build(recordType, permission_unit, data_record, newRecordElement):
         recordInfo = ET.SubElement(newRecordElement, 'recordInfo')
         validationType = ET.SubElement(recordInfo, 'validationType')
         ET.SubElement(validationType, 'linkedRecordType').text = 'validationType'
@@ -54,22 +57,6 @@ class  CommonData:
             ET.SubElement(permissionUnit, 'linkedRecordId').text= permission_unit
         oldId_fromSource = data_record.find('.//old_id')
         ET.SubElement(recordInfo, 'oldId').text = oldId_fromSource.text
-
-#    @staticmethod
-#    def recordInfoUnit_build(recordType, unit, data_record, newRecordElement):
-#        recordInfo = ET.SubElement(newRecordElement, 'recordInfo')
-#        validationType = ET.SubElement(recordInfo, 'validationType')
-#        ET.SubElement(validationType, 'linkedRecordType').text = 'validationType'
-#        ET.SubElement(validationType, 'linkedRecordId').text = 'diva-'+recordType
-#        dataDivider = ET.SubElement(recordInfo, 'dataDivider')
-#        ET.SubElement(dataDivider, 'linkedRecordType').text = 'system'
-#        ET.SubElement(dataDivider, 'linkedRecordId').text = 'divaData'
-#        if unit is not None:
-#            permissionUnit = ET.SubElement(recordInfo, 'permissionUnit')
-#            ET.SubElement(permissionUnit, 'linkedRecordType').text = 'permissionUnit'
-#            ET.SubElement(permissionUnit, 'linkedRecordId').text= unit
-#        oldIdFromSource = data_record.find('.//old_id')
-#        ET.SubElement(recordInfo, 'oldId').text = oldIdFromSource.text
     
     @staticmethod
     def get_oldId(data_record):
@@ -99,7 +86,7 @@ class  CommonData:
         ET.SubElement(topic, 'topic').text = topicLang_fromSource.text
 
     @staticmethod
-    def titleInfo_build(data_record, newRecordElement):
+    def title_info_build(data_record, newRecordElement):
         title_fromSource = data_record.find(f'.//title')
         if title_fromSource is not None and title_fromSource.text:
             titleInfo = ET.SubElement(newRecordElement, 'titleInfo')
@@ -107,6 +94,16 @@ class  CommonData:
         subTitle_fromSource = data_record.find(f'.//subTitle')
         if subTitle_fromSource is not None and subTitle_fromSource.text:
             ET.SubElement(titleInfo, 'subTitle').text = subTitle_fromSource.text
+            
+    @staticmethod
+    def titleInfo_alternative_build(data_record, new_record_element, titleType):
+        title_from_source = data_record.find('.//alternative_title')
+        if title_from_source is not None and title_from_source.text:
+            titleInfo = ET.SubElement(new_record_element, 'titleInfo', type = titleType)
+            ET.SubElement(titleInfo, 'title').text = title_from_source.text
+        subTitleFromSource = data_record.find('.//alterantive_sub_title')
+        if subTitleFromSource is not None and subTitleFromSource.text:
+            ET.SubElement(titleInfo, 'subTitle').text = subTitleFromSource.text
 
     @staticmethod
     def identifier_build(data_record, newRecordElement, identifierType, counter):
@@ -121,7 +118,7 @@ class  CommonData:
         return counter
     
     @staticmethod
-    def endDate_build(data_record, newRecordElement, originType):
+    def end_date_build(data_record, newRecordElement, originType):
         date_fromSource = data_record.find('.//end_date')
         if date_fromSource is not None and date_fromSource.text:
             year, month, day = map(str.strip, date_fromSource.text.split('-'))
@@ -149,3 +146,21 @@ class  CommonData:
         if url_fromSource is not None and url_fromSource.text:
             location = ET.SubElement(newRecordElement, 'location')
             ET.SubElement(location, 'url').text = url_fromSource.text
+    
+    @staticmethod
+    def note_build(data_record, newRecordElement, noteType):
+        note_fromSource = data_record.find(f'.//note_{noteType}')
+        if note_fromSource is not None and note_fromSource.text:
+            ET.SubElement(newRecordElement, 'note', type='external').text = note_fromSource.text
+        
+    @staticmethod
+    def genre_build(data_record, new_record_element, publication_map, counter):
+        genre_from_source = data_record.find('.//publication_type_id')
+        if genre_from_source is not None and genre_from_source.text:
+            genre_value = publication_map[genre_from_source.text]
+            ET.SubElement(new_record_element, 'genre', repeatId=str(counter), type = 'outputType').text = genre_value
+            counter += 1
+        return counter
+        
+        
+        
