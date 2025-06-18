d3209ab765b73847746320a51eea0b25890e9392
Leo Danielsson <leo.danielsson@syson.se>
2025-06-18 16:39:08 +0200

    fix create_outputs

--- scripts/create_outputs_varldskulturmuseerna.py
+++ scripts/create_outputs_varldskulturmuseerna.py
@@ -1,7 +1,11 @@
 from fedora_to_cora.output_create import transform_to_cora_output
 from common.xml_utils import pretty_print_xml
+from common.common_data import read_source_xml
 
-if __name__ == "__main__":
+def main():
     source_record = read_source_xml("data/fedora_xml/1681782_varldskulturmuserna.xml")
     cora_output = transform_to_cora_output(source_record)
     print(pretty_print_xml(cora_output))
+
+if __name__ == "__main__":
+    main()
