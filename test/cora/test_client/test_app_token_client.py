31b2b4ddff46b6bbc858c88d1f9aa071ed92edc0
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-06-26 14:34:46 +0200

    revert AppTokenClient test

--- test/cora/test_client/test_app_token_client.py
+++ test/cora/test_client/test_app_token_client.py
@@ -1,54 +1,206 @@
-import pytest
-from unittest.mock import MagicMock
+import unittest
+
 from cora.client.app_token_client import AppTokenClient
+from unittest.mock import MagicMock
+from cora.client.login_error import LoginError
 
 
-@pytest.fixture
-def setup_client():
-    mock_time = MagicMock()
-    mock_threading = MagicMock()
-    mock_requests = MagicMock()
+class TestAppTokenClient(unittest.TestCase):
 
-    login_url = 'https://cora.epc.ub.uu.se/diva/login/rest/apptoken'
-    login_id = 'divaAdmin@cora.epc.ub.uu.se'
-    app_token = '49ce00fb-68b5-4089-a5f7-1c225d3cf156'
-    
-    dependencies = {"requests": mock_requests,
-                    "time": mock_time,
-                    "threading": mock_threading}
-    login_spec = {"login_url": login_url,
-                  "login_id": login_id,
-                  "app_token": app_token}
-    client = AppTokenClient(dependencies)
-    return {
-        "mock_time": mock_time,
-        "mock_threading": mock_threading,
-        "mock_requests": mock_requests,
-        "login_url": login_url,
-        "login_id": login_id,
-        "app_token": app_token,
-        "dependencies": dependencies,
-        "login_spec": login_spec,
-        "client": client
-    }
+    def setUp(self):
+        self.mock_time = MagicMock()
+        self.mock_threading = MagicMock()
+        self.mock_requests = MagicMock()
 
-def test_mocks_are_created(setup_client):
-    assert setup_client["mock_time"] is not None
-    assert setup_client["mock_threading"] is not None
-    assert setup_client["mock_requests"] is not None
+        self.login_url = "https://cora.epc.ub.uu.se/diva/login/rest/apptoken"
+        self.login_id = "divaAdmin@cora.epc.ub.uu.se"
+        self.app_token = "49ce00fb-68b5-4089-a5f7-1c225d3cf156"
 
-def test_dependencies_are_set_correctly(setup_client):
-    deps = setup_client["dependencies"]
-    assert deps["requests"] is setup_client["mock_requests"]
-    assert deps["time"] is setup_client["mock_time"]
-    assert deps["threading"] is setup_client["mock_threading"]
+        self.dependencies = {
+            "requests": self.mock_requests,
+            "time": self.mock_time,
+            "threading": self.mock_threading,
+        }
+        self.login_spec = {
+            "login_url": self.login_url,
+            "login_id": self.login_id,
+            "app_token": self.app_token,
+        }
+        self.client = AppTokenClient(self.dependencies)
 
-def test_login_spec_is_set_correctly(setup_client):
-    spec = setup_client["login_spec"]
-    assert spec["login_url"] == setup_client["login_url"]
-    assert spec["login_id"] == setup_client["login_id"]
-    assert spec["app_token"] == setup_client["app_token"]
+    def tearDown(self):
+        pass
 
-def test_client_is_initialized(setup_client):
-    assert setup_client["client"] is not None
-    assert isinstance(setup_client["client"], AppTokenClient)
+    def test_init(self):
+        self.assertIsNotNone(self.client)
+
+    def test_login(self):
+        self.set_up_response_answer(201, self.get_ok_login_answer())
+        login_headers = {
+            "Content-Type": "application/vnd.cora.login",
+            "Accept": "application/vnd.cora.authentication+json",
+        }
+
+        self.client.login(self.login_spec)
+
+        combined_login_id_app_token = self.login_id + "\n" + self.app_token
+        self.mock_requests.post.assert_called_once_with(
+            self.login_url, data=combined_login_id_app_token, headers=login_headers
+        )
+
+    def test_login_schedule_token_refresh(self):
+        self.set_up_response_answer(201, self.get_ok_login_answer())
+        self.set_up_mock_time_for_ten_minutes_before_renew_until()
+        mock_timer = MagicMock()
+        self.mock_threading.Timer.return_value = mock_timer
+        login_renew_before_expires_time = 20
+
+        self.client.login(self.login_spec)
+
+        ten_minutes_minus_20_seconds = 10 * 60 - login_renew_before_expires_time
+        self.mock_threading.Timer.assert_called_once_with(
+            ten_minutes_minus_20_seconds,
+            self.client._get_new_token,
+            args=[self.get_ok_login_answer()["authentication"]["actionLinks"]["renew"]],
+        )
+        mock_timer.start.assert_called_once()
+
+    def test__get_new_token(self):
+        self.set_up_response_answer(200, self.get_ok_login_answer())
+        captured = {}
+
+        def fake_handle(captured_response):
+            captured["response"] = captured_response
+
+        self.client.get_auth_token = lambda: "fakeToken"
+        self.client._handle_login_response = fake_handle
+
+        renew = self.get_ok_login_answer()["authentication"]["actionLinks"]["renew"]
+        url = renew["url"]
+        accept = renew["accept"]
+        auth_token = "fakeToken"
+        headers = {"Accept": accept, "authToken": auth_token}
+
+        self.client._get_new_token(renew)
+
+        self.mock_requests.post.assert_called_once_with(url, headers=headers)
+        self.assertEqual(captured["response"], self.mock_requests.post.return_value)
+
+    def test_get_auth_token_create(self):
+        self.set_up_response_answer(201, self.get_ok_login_answer())
+
+        self.client.login(self.login_spec)
+
+        self.assertEqual(
+            self.client.get_auth_token(), "5545e11c-7e90-4289-87d0-155bb2078f85"
+        )
+
+    def test_get_auth_token_renew(self):
+        self.set_up_response_answer(200, self.get_ok_login_answer())
+
+        self.client.login(self.login_spec)
+
+        self.assertEqual(
+            self.client.get_auth_token(), "5545e11c-7e90-4289-87d0-155bb2078f85"
+        )
+
+    def test_get_auth_token_response_code_not_201(self):
+        self.set_up_response_answer(401, "not logged in")
+
+        with self.assertRaises(LoginError) as context:
+            self.client.login(self.login_spec)
+
+        login_error = context.exception
+        self.assertIsInstance(login_error, LoginError)
+        self.assertEqual("Login failed", str(login_error))
+
+        original_exception = login_error.original_exception
+        self.assertIsInstance(original_exception, LoginError)
+        self.assertEqual(str(original_exception), "Login failed: Expected 201, got 401")
+        self.assertIsInstance(login_error.__cause__, LoginError)
+
+    def test_get_auth_token_answer_not_parsable_error(self):
+        self.set_up_response_answer(201, "not ok login")
+
+        with self.assertRaises(LoginError) as context:
+            self.client.login(self.login_spec)
+
+        login_error = context.exception
+        self.assertIsInstance(login_error, LoginError)
+        self.assertEqual("Login failed", str(login_error))
+
+        original_exception = login_error.original_exception
+        self.assertIsInstance(original_exception, TypeError)
+        self.assertEqual(
+            str(original_exception), "string indices must be integers, not 'str'"
+        )
+        self.assertIsInstance(login_error.__cause__, TypeError)
+
+    def set_up_response_answer(self, status_code, login_answer):
+        mock_response = MagicMock()
+        mock_response.status_code = status_code
+        mock_response.json.return_value = login_answer
+        self.mock_requests.post.return_value = mock_response
+
+    def set_up_mock_time_for_ten_minutes_before_renew_until(self):
+        time_from_valid_until_in_response = 1744703967172
+        ten_minutes = 10
+        valid_until_minus_ten_minutes = (
+            time_from_valid_until_in_response - ten_minutes * 60 * 1000
+        )
+        valid_until_minus_ten_minutes_sec = valid_until_minus_ten_minutes / 1000
+        self.mock_time.time.return_value = valid_until_minus_ten_minutes_sec
+
+    def get_ok_login_answer(self):
+        return {
+            "authentication": {
+                "data": {
+                    "children": [
+                        {
+                            "name": "token",
+                            "value": "5545e11c-7e90-4289-87d0-155bb2078f85",
+                        },
+                        {"name": "validUntil", "value": "1744703967172"},
+                        {"name": "renewUntil", "value": "1744789767172"},
+                        {"name": "userId", "value": "161616"},
+                        {"name": "loginId", "value": "divaAdmin@cora.epc.ub.uu.se"},
+                        {"name": "firstName", "value": "DiVA"},
+                        {"name": "lastName", "value": "Admin"},
+                        {
+                            "repeatId": "1",
+                            "children": [
+                                {"name": "linkedRecordType", "value": "permissionUnit"},
+                                {"name": "linkedRecordId", "value": "test1"},
+                            ],
+                            "name": "permissionUnit",
+                        },
+                        {
+                            "repeatId": "2",
+                            "children": [
+                                {"name": "linkedRecordType", "value": "permissionUnit"},
+                                {"name": "linkedRecordId", "value": "test2"},
+                            ],
+                            "name": "permissionUnit",
+                        },
+                    ],
+                    "name": "authToken",
+                },
+                "actionLinks": {
+                    "renew": {
+                        "requestMethod": "POST",
+                        "rel": "renew",
+                        "url": "http://localhost:38182/login/rest/authToken/39d029d1-1262-42a7-82df-a0e64daeb3ec",
+                        "accept": "application/vnd.cora.authentication+json",
+                    },
+                    "delete": {
+                        "requestMethod": "DELETE",
+                        "rel": "delete",
+                        "url": "http://localhost:38182/login/rest/authToken/39d029d1-1262-42a7-82df-a0e64daeb3ec",
+                    },
+                },
+            }
+        }
+
+
+if __name__ == "__main__":
+    unittest.main()
