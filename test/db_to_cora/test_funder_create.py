f2f4cc5179f08900332602c4417148e6d759a8b5
Sara Tobiasson <sara.tobiasson@ub.uu.se>
2025-08-13 15:09:48 +0200

    add test for required elements

--- test/db_to_cora/test_funder_create.py
+++ test/db_to_cora/test_funder_create.py
@@ -3,7 +3,42 @@ from db_to_cora.funder_transform import transform_funder
 from common.test_helper import assert_equal_for_xml_and_xml_string
 
 
-def test_element_xml():
+def test_required_xml():
+    source_record = ET.fromstring(
+        """
+        <DATA_RECORD>
+            <old_id>1234</old_id>
+            <name_swe>Ett namn</name_swe>
+        </DATA_RECORD>       
+        """
+    )
+    
+    result = transform_funder(source_record)
+
+    expected_xml = """
+    <funder>
+        <recordInfo>
+            <validationType>
+                <linkedRecordType>validationType</linkedRecordType>
+                <linkedRecordId>diva-funder</linkedRecordId>
+            </validationType>
+            <dataDivider>
+                <linkedRecordType>system</linkedRecordType>
+                <linkedRecordId>divaData</linkedRecordId>
+            </dataDivider>
+            <oldId>1234</oldId>
+        </recordInfo>
+        <authority lang="swe">
+            <name type="corporate">
+                <namePart>Ett namn</namePart>
+            </name>
+        </authority>
+    </funder>
+    """
+
+    assert_equal_for_xml_and_xml_string(result, expected_xml)
+
+def test_complete_xml():
     source_record = ET.fromstring(
         """
         <DATA_RECORD>
@@ -53,3 +88,5 @@ def test_element_xml():
     """
 
     assert_equal_for_xml_and_xml_string(result, expected_xml)
+    
+    
