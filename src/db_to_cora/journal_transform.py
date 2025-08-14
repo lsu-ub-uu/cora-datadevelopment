6f541736bc00738872350bb1270dc2263b6f0131
Sara Tobiasson <sara.tobiasson@ub.uu.se>
2025-08-14 14:41:47 +0200

    added originInfo, identifier and location

--- src/db_to_cora/journal_transform.py
+++ src/db_to_cora/journal_transform.py
@@ -1,7 +1,10 @@
 import xml.etree.ElementTree as ET
-#from common.xml_utils import append_if_value
+from common.xml_utils import append_if_value
 from common.record_info_create import record_info_create
 from common.common_data import create_title_info
+from common.common_data import create_origin_info
+from common.common_data import create_identifiers_from_source
+from common.common_data import create_location
 
 nameInData = "journal"
 
@@ -15,6 +18,11 @@ def transform_journal(source_record: ET.Element) -> ET.Element:
     
     journal.append(_create_record_info(source_record))
     journal.append(_create_title_info(source_record))
+    append_if_value(journal, _create_origin_info(source_record, origin_type="originInfo"))
+    append_if_value(journal, _create_identifiers_from_source(source_record, identifier_type="eissn"))
+    append_if_value(journal, _create_identifiers_from_source(source_record, identifier_type="pissn"))
+    append_if_value(journal, _create_location(source_record))
+    
         
     return journal
 
@@ -33,8 +41,26 @@ def _create_record_info(source_record: ET.Element) -> ET.Element:
     
 def _create_title_info(source_record: ET.Element) -> ET.Element:
     title = source_record.find(f".//title")
+    subtitle = source_record.find(f".//subtitle")
     if title is not None and title.text:
         return create_title_info(
-            title.text)
+            title.text, subtitle)
     
+def _create_origin_info(source_record: ET.Element, origin_type: str) -> ET.Element | None:
+    end_date = source_record.find(".//end_date")
+    if end_date is not None and end_date.text:
+        return create_origin_info(
+            end_date.text, origin_type)
+        
+def _create_identifiers_from_source(source_record: ET.Element, identifier_type: str) -> ET.Element | None:
+    identifier = source_record.find(f".//identifier_{identifier_type}")
+    if identifier is not None and identifier.text:
+        return create_identifiers_from_source(
+            identifier.text, identifier_type)
+
     
+def _create_location(source_record: ET.Element) -> ET.Element | None:
+    url = source_record.find(".//url")
+    if url is not None and url.text:
+        return create_location(
+            url.text)
--- test/db_to_cora/test_journal_create.py
+++ test/db_to_cora/test_journal_create.py
@@ -2,12 +2,13 @@ import xml.etree.ElementTree as ET
 from db_to_cora.journal_transform import transform_journal
 from common.test_helper import assert_equal_for_xml_and_xml_string
 
-def test_element_xml():
+def test_required_xml():
     source_record = ET.fromstring(
         """
         <DATA_RECORD>
             <old_id>1234</old_id>
             <title>some title</title>
+            <subtitle>some subtitle</subtitle>
         </DATA_RECORD>       
         """
     )
@@ -29,7 +30,59 @@ def test_element_xml():
         </recordInfo>
         <titleInfo>
             <title>some title</title>
+            <subtitle> some subtitle</subtitle>
         </titleInfo>
+    </journal>
+    """
+
+    assert_equal_for_xml_and_xml_string(result, expected_xml)
+    
+def test_complete_xml():
+    source_record = ET.fromstring(
+        """
+        <DATA_RECORD>
+            <old_id>1234</old_id>
+            <title>some title</title>
+            <subtitle> some subtitle</subtitle>
+            <end_date>2025-05-06</end_date>
+            <url>url.se</url>
+            <identifier_eissn>1234-1234</identifier_eissn>
+            <identifier_pissn>5678-5678</identifier_pissn>
+        </DATA_RECORD>
+        """
+    )
+
+    result = transform_journal(source_record)
+
+    expected_xml = """
+    <journal>
+        <recordInfo>
+            <validationType>
+                <linkedRecordType>validationType</linkedRecordType>
+                <linkedRecordId>diva-journal</linkedRecordId>
+            </validationType>
+            <dataDivider>
+                <linkedRecordType>system</linkedRecordType>
+                <linkedRecordId>divaData</linkedRecordId>
+            </dataDivider>
+            <oldId>1234</oldId>
+        </recordInfo>
+        <titleInfo>
+            <title>some title</title>
+            <subtitle> some subtitle</subtitle>
+        </titleInfo>
+        <originInfo>
+            <dateIssued point="end">
+                <year>2025</year>
+                <month>05</month>
+                <day>06</day>
+            </dateIssued>
+        </originInfo>
+        <identifier displayLabel="eissn" repeatId="0" type="issn">1234-1234</identifier>
+        <identifier displayLabel="pissn" repeatId="1" type="issn">5678-5678</identifier>
+        <location>
+            <url>url.se</url>
+        </location>
     </journal>
     """
 
