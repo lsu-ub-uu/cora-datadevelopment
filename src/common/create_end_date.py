df1169abaf81f97750c6db0e044f93e421f9c676
Sara Tobiasson <sara.tobiasson@ub.uu.se>
2025-08-14 14:39:38 +0200

    updated handling of year, month, day

--- src/common/create_end_date.py
+++ src/common/create_end_date.py
@@ -1,29 +1,29 @@
 import xml.etree.ElementTree as ET
 
-
-def create_end_date(date: str, origin_type: str | None = None) -> ET.Element:
+def create_end_date(date: str) -> ET.Element:
     """
     Create a Cora end date element from a source record.
     """
+    
     end_date = ET.Element("endDate")
     year, month, day = map(str.strip, date.split("-"))
     
-    end_date.append(
-        create_element("year", year),
-        )
-    end_date.append(
-        create_element("month", month),
-        )
-    end_date.append(
-        create_element("day", day),
-        )
-    
+    append_year_month_day(end_date, year, month, day)
+
     return end_date
 
-
+def append_year_month_day(element: ET.Element, year: str, month: str, day:str):
+    element.append(
+        create_element("year", year))
+    element.append(
+        create_element("month", month))
+    element.append(
+        create_element("day", day))
+    
+    
 def create_element(tag_name: str, text: str) -> ET.Element:
     element = ET.Element(tag_name)
     element.text = text
     
     return element
-
+    
