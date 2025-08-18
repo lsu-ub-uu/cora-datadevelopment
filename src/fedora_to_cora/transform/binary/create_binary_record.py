a98bb3b207cd1acc33689c34c55043537c29e65d
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-08-18 16:54:47 +0200

    implement binary record creation

--- src/fedora_to_cora/transform/binary/create_binary_record.py
+++ src/fedora_to_cora/transform/binary/create_binary_record.py
@@ -1,7 +1,36 @@
 import xml.etree.ElementTree as ET
+from common.record_info_create import record_info_create
+from common.xml_utils import append_if_value
+from .get_binary_visibility import get_binary_visibility
 
 
 def create_binary_record(source_record: ET.Element) -> ET.Element:
     binary_record = ET.Element("binary")
-    # Add relevant information from the source_record to the binary_record
+
+    binary_record.append(
+        record_info_create(
+            validation_type_id="genericBinary",
+            visibility=get_binary_visibility(source_record),
+        )
+    )
+
+    append_if_value(binary_record, _create_original_file_name(source_record))
+    append_if_value(binary_record, _create_expected_file_size(source_record))
+
     return binary_record
+
+
+def _create_original_file_name(source_record: ET.Element) -> ET.Element:
+    original_file_name = ET.Element("originalFileName")
+    path = source_record.findtext("./path")
+    if path is not None:
+        original_file_name.text = path.split("/")[-1]
+    return original_file_name
+
+
+def _create_expected_file_size(source_record: ET.Element) -> ET.Element:
+    expected_file_size = ET.Element("expectedFileSize")
+    file_size = source_record.findtext("./fileSize")
+    if file_size is not None:
+        expected_file_size.text = file_size
+    return expected_file_size
