b834b972aeb8783033efa752563b484e2cc108b1
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-08-22 15:08:55 +0200

    add attachment version, secrecy and registration number to attachment mapping

--- src/fedora_to_cora/transform/attachment_transform.py
+++ src/fedora_to_cora/transform/attachment_transform.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 from common.common_data import create_record_link_using_name_type_id
+from common.xml_utils import append_if_value
 from fedora_to_cora.transform.binary.get_attachment_type import get_attachment_type
 
 
@@ -14,9 +15,50 @@ def attachment_transform(
             record_id=binary_record_id,
         )
     )
+
     ET.SubElement(attachment, "type").text = get_attachment_type(source_attachment)
 
-    admin_info = ET.SubElement(attachment, "adminInfo")
-    ET.SubElement(admin_info, "availability").text = "availableNow"
+    attachment_version = _get_attachment_version(source_attachment)
+    if attachment_version is not None:
+        ET.SubElement(attachment, "note", type="attachmentVersion").text = (
+            attachment_version
+        )
+
+    append_if_value(attachment, _create_admin_info(source_attachment))
 
     return attachment
+
+
+def _get_attachment_version(source_attachment: ET.Element) -> str | None:
+    prePrint = source_attachment.findtext("./prePrint")
+    postPrint = source_attachment.findtext("./postPrint")
+    print_ = source_attachment.findtext("./print")
+
+    # Validate at most one tag is set to "true"
+    true_count = sum(1 for value in [prePrint, postPrint, print_] if value == "true")
+    if true_count > 1:
+        raise ValueError("Multiple attachment versions found")
+
+    if prePrint == "true":
+        return "submitted"
+    elif postPrint == "true":
+        return "accepted"
+    elif print_ == "true":
+        return "published"
+    return None
+
+
+def _create_admin_info(source_attachment: ET.Element) -> ET.Element:
+    admin_info = ET.Element("adminInfo")
+    ET.SubElement(admin_info, "availability").text = "availableNow"
+
+    if source_attachment.findtext("secrecyInfo/secrecy") == "true":
+        ET.SubElement(admin_info, "secrecy").text = "true"
+
+    registration_number = source_attachment.findtext("./registrationNumber")
+    if registration_number is not None:
+        ET.SubElement(admin_info, "identifier", type="registrationNumber").text = (
+            registration_number
+        )
+
+    return admin_info
--- test/fedora_to_cora/transform/test_attachment_transform.py
+++ test/fedora_to_cora/transform/test_attachment_transform.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock
 import xml.etree.ElementTree as ET
 from common.test_helper import assert_equal_for_xml_and_xml_string
 from fedora_to_cora.transform.attachment_transform import attachment_transform
+import pytest
 
 
 def test_attachment_transform(monkeypatch):
@@ -40,16 +41,112 @@ def test_attachment_transform(monkeypatch):
         </attachment>
         """,
     )
-    # <type>
-    # <displayLabel>
-    # <note type"attachmentVersion">
-    # <adminInfo>
-    #     <availability>
-    #         <year>
-    #         <month>
-    #         <day>
-    #     </availability>
-    #     <secrecy>
-    #     <identifier type="registrationNumber">
-    #     <note type:"attachment">
-    # </adminInfo>
+
+
+@pytest.mark.parametrize(
+    "tagName,expected_attachment_version",
+    [
+        ("prePrint", "submitted"),
+        ("postPrint", "accepted"),
+        ("print", "published"),
+    ],
+)
+def test_attachment_version_submitted_when_preprint(
+    tagName, expected_attachment_version
+):
+    source_record = ET.fromstring(
+        f"""
+            <attachment>
+                <fileLabel>
+                    <fileLabelId>50</fileLabelId>
+                </fileLabel>
+                <path>test.pdf</path>
+                <{tagName}>true</{tagName}>
+            </attachment>
+        """
+    )
+
+    binary_record_id = "binary:12345"
+
+    attachment = attachment_transform(source_record, binary_record_id)
+
+    attachment_version = attachment.findtext("note[@type='attachmentVersion']")
+    assert attachment_version == expected_attachment_version
+
+
+def test_raises_error_when_multiple_attachment_versions():
+    source_record = ET.fromstring(
+        """
+        <attachment>
+            <fileLabel>
+                <fileLabelId>50</fileLabelId>
+            </fileLabel>
+            <path>test.pdf</path>
+            <prePrint>true</prePrint>
+            <postPrint>true</postPrint>
+        </attachment>
+        """
+    )
+
+    binary_record_id = "binary:12345"
+
+    with pytest.raises(ValueError, match="Multiple attachment versions found"):
+        attachment_transform(source_record, binary_record_id)
+
+
+def test_secrecy():
+    source_record = ET.fromstring(
+        """
+        <attachment>
+            <fileLabel>
+                <fileLabelId>50</fileLabelId>
+            </fileLabel>
+            <path>test.pdf</path>
+            <secrecyInfo>
+                <secrecy>true</secrecy>
+            </secrecyInfo>
+        </attachment>
+        """
+    )
+
+    binary_record_id = "binary:12345"
+
+    attachment = attachment_transform(source_record, binary_record_id)
+
+    assert attachment.findtext("./adminInfo/secrecy") == "true"
+
+
+def test_registration_number():
+    source_record = ET.fromstring(
+        """
+        <attachment>
+            <fileLabel>
+                <fileLabelId>50</fileLabelId>
+            </fileLabel>
+            <path>test.pdf</path>
+            <registrationNumber>1234</registrationNumber>
+        </attachment>
+        """
+    )
+
+    binary_record_id = "binary:12345"
+
+    attachment = attachment_transform(source_record, binary_record_id)
+
+    assert (
+        attachment.findtext("./adminInfo/identifier[@type='registrationNumber']")
+        == "1234"
+    )
+
+
+# <displayLabel>
+# <note type="attachmentVersion">
+# <adminInfo>
+#     <availability>
+#     <dateAvailability>
+#         <year>
+#         <month>
+#         <day>
+#     </availability>
+#     <note type="attachment">
+# </adminInfo>
