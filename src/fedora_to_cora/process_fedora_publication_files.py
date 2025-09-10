a9630b1d5cc0f05a92f90f40ab5a859ea4a785a6
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-09-10 11:29:48 +0200

    Add source data validation to output migrate
    Co-authored-by: egiiiiil <egiiiiil@users.noreply.github.com>

--- src/fedora_to_cora/process_fedora_publication_files.py
+++ src/fedora_to_cora/process_fedora_publication_files.py
@@ -3,38 +3,78 @@ import os
 from common.threads import run_with_threads
 from cora.context import CoraContext, Context
 from fedora_to_cora.output_migrate import output_migrate
+from common.xml_validate import validate_xml, XMLValidationError
+from fedora_to_cora.fedora_publication_spec import fedora_publication_xml_spec
+import xml.etree.ElementTree as ET
 
 
 def process_fedora_publication_files(
     xml_dir: str, context: Context, apply: bool = False
 ):
-    successful_transformations = []
-    failed_transformations = []
-
     context.log("==== Begin processing Fedora XML publications ====")
     context.log(
         f"==== xml_dir={xml_dir}, system={context.get_system()}, apply={apply} ===="
     )
-    context.log("==================================================")
+    context.log("=" * 50)
 
-    files = [filename for filename in os.listdir(xml_dir) if filename.endswith(".xml")]
+    source_records = _read_source_records(xml_dir)
 
-    def process_file(filename):
-        context.log(f"--- Processing file: {filename} ---")
+    source_records_valid = _validate_source_records(source_records, context)
+
+    if source_records_valid:
+        _migrate_records(source_records, context, xml_dir, apply)
+
+    print(f"Output logged to {context.get_logger().handlers[0].baseFilename}")  # type: ignore[attr-defined]
+
+
+def _read_source_records(xml_dir):
+    return [
+        read_source_xml(os.path.join(xml_dir, filename))
+        for filename in os.listdir(xml_dir)
+        if filename.endswith(".xml")
+    ]
+
+
+def _validate_source_records(source_records, context: Context) -> bool:
+    validation_errors = []
+    for source_record in source_records:
         try:
-            source_record = _read_source_record_from_file(xml_dir, filename)
+            validate_xml(source_record, fedora_publication_xml_spec)
+        except XMLValidationError as e:
+            pid = source_record.findtext("pid")
+            validation_errors.append(f"{pid} - XML Validation Error: {str(e)}")
+    if len(validation_errors) > 0:
+        context.log(
+            "==== Skipped migration due to XML Validation Error in source data ==== "
+        )
+        for error in validation_errors:
+            context.log(f"❌ {error}")
+        return False
+    return True
+
+
+def _migrate_records(
+    source_records: list[ET.Element], context: Context, xml_dir: str, apply: bool
+):
+    successful_transformations = []
+    failed_transformations = []
+
+    def process_file(source_record):
+        pid = source_record.findtext("pid")
+        context.log(f"--- Processing record with pid: {pid} ---")
+        try:
             valid, errors = output_migrate(source_record, context, xml_dir, apply)
             if valid:
-                successful_transformations.append(filename)
+                successful_transformations.append(pid)
             else:
                 failed_transformations.append(
-                    f"{filename} - Errors: [{', '.join(errors) if errors else ''}]"
+                    f"{pid} - Errors: [{', '.join(errors) if errors else ''}]"
                 )
         except Exception as e:
-            failed_transformations.append(f"{filename} - Exception: {str(e)}")
+            failed_transformations.append(f"{pid} - Exception: {str(e)}")
 
     run_with_threads(
-        files,
+        source_records,
         process_file,
         workers=8,
         desc="Processing publication files",
@@ -43,20 +83,12 @@ def process_fedora_publication_files(
     context.log("==== Processing complete ====")
 
     context.log(f"{len(successful_transformations)} Successful transformations:")
-    for filename in successful_transformations:
-        context.log(f"✅ {filename}")
+    for pid in successful_transformations:
+        context.log(f"✅ {pid}")
 
     context.log(f"{len(failed_transformations)} Failed transformations:")
-    for filename in failed_transformations:
-        context.log(f"❌ {filename}")
-
+    for error in failed_transformations:
+        context.log(f"❌ {error}")
     print(
         f"{len(successful_transformations)} succeeded, {len(failed_transformations)} failed."
     )
-    print(f"Output logged to {context.get_logger().handlers[0].baseFilename}")  # type: ignore[attr-defined]
-
-
-def _read_source_record_from_file(xml_dir, filename):
-    filepath = os.path.join(xml_dir, filename)
-    source_record = read_source_xml(filepath)
-    return source_record
--- test/fedora_to_cora/test_process_fedora_publication_files.py
+++ test/fedora_to_cora/test_process_fedora_publication_files.py
@@ -1,88 +1,130 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from xml.etree import ElementTree as ET
+from common.xml_validate import XMLValidationError
 from cora.context import MockContext
 from fedora_to_cora.process_fedora_publication_files import (
     process_fedora_publication_files,
 )
 
 
-def test_process_fedora_publication_files(monkeypatch):
+@patch("fedora_to_cora.process_fedora_publication_files.output_migrate")
+@patch("fedora_to_cora.process_fedora_publication_files.run_with_threads")
+@patch("os.listdir")
+@patch("fedora_to_cora.process_fedora_publication_files.read_source_xml")
+@patch("fedora_to_cora.process_fedora_publication_files.validate_xml")
+def test_process_fedora_publication_files(
+    mock_validate_xml,
+    mock_read_source_xml,
+    mock_listdir,
+    mock_run_with_threads,
+    mock_output_migrate,
+):
     xml_dir = "test/xml"
     mock_context = MockContext()
     apply = False
 
-    mock_read_source_xml = MagicMock(return_value=ET.Element("publication"))
-    monkeypatch.setattr(
-        "fedora_to_cora.process_fedora_publication_files.read_source_xml",
-        mock_read_source_xml,
-    )
+    mock_read_source_xml.side_effect = [
+        ET.fromstring("<publication><pid>test1</pid></publication>"),
+        ET.fromstring("<publication><pid>test2</pid></publication>"),
+        ET.fromstring("<publication><pid>test3</pid></publication>"),
+    ]
+    mock_listdir.return_value = ["test1.xml", "test2.xml", "test3.xml"]
+    mock_run_with_threads.side_effect = lambda items, func, workers, desc: [
+        func(item) for item in items
+    ]
 
-    monkeypatch.setattr(
-        "os.listdir", MagicMock(return_value=["test1.xml", "test2.xml", "test3.xml"])
-    )
-
-    monkeypatch.setattr(
-        "fedora_to_cora.process_fedora_publication_files.run_with_threads",
-        lambda items, func, workers, desc: [func(item) for item in items],
-    )
-
-    output_migrate_mock = MagicMock(
-        side_effect=[
-            (True, []),  # test1.xml - success
-            (False, ["validation error"]),  # test2.xml - failure
-            (True, []),  # test3.xml - success
-        ]
-    )
-    monkeypatch.setattr(
-        "fedora_to_cora.process_fedora_publication_files.output_migrate",
-        output_migrate_mock,
-    )
+    mock_output_migrate.side_effect = [
+        (True, []),  # test1.xml - success
+        (False, ["validation error"]),  # test2.xml - failure
+        (True, []),  # test3.xml - success
+    ]
 
     process_fedora_publication_files(xml_dir, mock_context, apply)
 
-    assert output_migrate_mock.call_count == 3
+    assert mock_output_migrate.call_count == 3
 
-    mock_context.log.assert_any_call("✅ test1.xml")  # type: ignore
-    mock_context.log.assert_any_call("❌ test2.xml - Errors: [validation error]")  # type: ignore
-    mock_context.log.assert_any_call("✅ test3.xml")  # type: ignore
+    mock_context.log.assert_any_call("✅ test1")  # type: ignore
+    mock_context.log.assert_any_call("❌ test2 - Errors: [validation error]")  # type: ignore
+    mock_context.log.assert_any_call("✅ test3")  # type: ignore
 
 
-def test_handles_raised_exception_in_processing(monkeypatch):
+@patch("fedora_to_cora.process_fedora_publication_files.output_migrate")
+@patch("fedora_to_cora.process_fedora_publication_files.run_with_threads")
+@patch("os.listdir")
+@patch("fedora_to_cora.process_fedora_publication_files.read_source_xml")
+@patch("fedora_to_cora.process_fedora_publication_files.validate_xml")
+def test_handles_raised_exception_in_processing(
+    mock_validate_xml,
+    mock_read_source_xml,
+    mock_listdir,
+    mock_run_with_threads,
+    mock_output_migrate,
+):
     xml_dir = "test/xml"
     apply = False
-
-    mock_read_source_xml = MagicMock(return_value=ET.Element("publication"))
-    monkeypatch.setattr(
-        "fedora_to_cora.process_fedora_publication_files.read_source_xml",
-        mock_read_source_xml,
-    )
     mock_context = MockContext()
 
-    monkeypatch.setattr(
-        "os.listdir", MagicMock(return_value=["test1.xml", "test2.xml", "test3.xml"])
-    )
+    mock_read_source_xml.side_effect = [
+        ET.fromstring("<publication><pid>test1</pid></publication>"),
+        ET.fromstring("<publication><pid>test2</pid></publication>"),
+        ET.fromstring("<publication><pid>test3</pid></publication>"),
+    ]
+    mock_listdir.return_value = ["test1.xml", "test2.xml", "test3.xml"]
+    mock_run_with_threads.side_effect = lambda items, func, workers, desc: [
+        func(item) for item in items
+    ]
 
-    monkeypatch.setattr(
-        "fedora_to_cora.process_fedora_publication_files.run_with_threads",
-        lambda items, func, workers, desc: [func(item) for item in items],
-    )
+    mock_output_migrate.side_effect = [
+        (True, []),
+        Exception("Something went wrong during migration"),
+        (True, []),
+    ]
 
-    output_migrate_mock = MagicMock(
-        side_effect=[
-            (True, []),
-            Exception("Something went wrong during migration"),
-            (True, []),
-        ]
-    )
-    monkeypatch.setattr(
-        "fedora_to_cora.process_fedora_publication_files.output_migrate",
-        output_migrate_mock,
+    process_fedora_publication_files(xml_dir, mock_context, apply)
+
+    assert mock_output_migrate.call_count == 3
+
+    mock_context.log.assert_any_call("✅ test1")  # type: ignore
+    mock_context.log.assert_any_call("❌ test2 - Exception: Something went wrong during migration")  # type: ignore
+    mock_context.log.assert_any_call("✅ test3")  # type: ignore
+
+
+@patch("fedora_to_cora.process_fedora_publication_files.output_migrate")
+@patch("fedora_to_cora.process_fedora_publication_files.run_with_threads")
+@patch("os.listdir")
+@patch("fedora_to_cora.process_fedora_publication_files.read_source_xml")
+@patch("fedora_to_cora.process_fedora_publication_files.validate_xml")
+def test_does_not_start_migrating_when_any_xml_validation_fails(
+    mock_validate_xml,
+    mock_read_source_xml,
+    mock_listdir,
+    mock_run_with_threads,
+    mock_output_migrate,
+):
+    xml_dir = "test/xml"
+    apply = False
+    mock_context = MockContext()
+
+    mock_read_source_xml.side_effect = [
+        ET.fromstring("<publication><pid>test1</pid></publication>"),
+        ET.fromstring("<publication><pid>test2</pid></publication>"),
+        ET.fromstring("<publication><pid>test3</pid></publication>"),
+    ]
+    mock_listdir.return_value = ["test1.xml", "test2.xml", "test3.xml"]
+    mock_run_with_threads.side_effect = lambda items, func, workers, desc: [
+        func(item) for item in items
+    ]
+
+    mock_validate_xml.side_effect = (
+        None,
+        XMLValidationError("Some xml validation error"),
+        XMLValidationError("Some other xml validation error"),
     )
 
     process_fedora_publication_files(xml_dir, mock_context, apply)
 
-    assert output_migrate_mock.call_count == 3
-
-    mock_context.log.assert_any_call("✅ test1.xml")  # type: ignore
-    mock_context.log.assert_any_call("❌ test2.xml - Exception: Something went wrong during migration")  # type: ignore
-    mock_context.log.assert_any_call("✅ test3.xml")  # type: ignore
+    assert mock_validate_xml.call_count == 3
+    assert mock_output_migrate.call_count == 0
+    mock_context.log.assert_any_call("==== Skipped migration due to XML Validation Error in source data ==== ")  # type: ignore
+    mock_context.log.assert_any_call("❌ test2 - XML Validation Error: Some xml validation error")  # type: ignore
+    mock_context.log.assert_any_call("❌ test3 - XML Validation Error: Some other xml validation error")  # type: ignore
