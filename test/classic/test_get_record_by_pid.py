55387dbbd759469e9767c79abf7fbbcd8820cf3e
Marcus Viden <marcus.viden@ub.uu.se>
2025-10-06 13:50:23 +0200

    Fix broken test

--- test/classic/test_get_record_by_pid.py
+++ test/classic/test_get_record_by_pid.py
@@ -1,11 +1,13 @@
+import re
+
 from classic.get_record_by_pid import get_record_by_pid
 from common.test_helper import assert_equal_for_xml_and_xml_string
 
 
 def test_get_record_by_pid(requests_mock):
     pid = "some-pid"
-    requests_mock.get(
-        f"https://uu.diva-portal.org:8443/fedora/get/{pid}/MODEL_NOREF",
+    requests_mock.get(re.compile(
+        f"https?://[^/]+/fedora/get/{pid}/MODEL_NOREF"),
         text="<record></record>",
     )
     result = get_record_by_pid(pid)
