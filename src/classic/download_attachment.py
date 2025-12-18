b90d489d85afab1fbce43355abbaa2ffa37b3bd2
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-12-18 11:54:29 +0100

    CORA-3254 Update to use content instead of raw

--- src/classic/download_attachment.py
+++ src/classic/download_attachment.py
@@ -5,4 +5,4 @@ def download_attachment(pid: str, file_name: str):
     download_url = f"http://localhost:8088/fedora/get/{pid}/{file_name}"
     download_response = requests.get(download_url, stream=True)
     download_response.raise_for_status()
-    return download_response.raw
+    return download_response.content
