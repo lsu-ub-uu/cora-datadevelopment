53446718dcb79ba333260f472c97ad4c85c86e7c
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-01-13 15:58:59 +0100

    Update binary migration to not stream

--- src/fedora_to_cora/binary_migrate.py
+++ src/fedora_to_cora/binary_migrate.py
@@ -10,40 +10,48 @@ def migrate_binary(binary_record: ET.Element, pid: str, file_name: str, context:
     start_migrate = time.perf_counter()
     context.log(f"[PID {pid}] ⏳ Starting migrate file from Fedora: {download_url}")
     
-    with requests.get(download_url, stream=True) as download_response:
-        download_response.raise_for_status()
-        multipart_data = MultipartEncoder(
-            fields={
-                "file": (
-                    file_name,
-                    download_response.raw,
-                    "application/octet-stream",
-                )
-            }
-        )
-
-        upload_url = _get_upload_url(binary_record)
-
-        upload_response = requests.post(
-            upload_url,
-            data=multipart_data,
-            headers={
-                "Authtoken": context.get_auth_token(),
-                "Content-Type": multipart_data.content_type,
-            },
-            stream=True
-        )
-        
-        if upload_response.status_code != 200:
-            context.log(f"[PID {pid}] Upload failed: {upload_response.text}")
-            raise UploadError(
-                f"Failed to upload binary file '{file_name}': {upload_response.status_code} - {upload_response.text}"
+    start_download = time.perf_counter()
+    download_response = requests.get(download_url)
+    download_response.raise_for_status()
+    binary_data = download_response.content
+    end_download = time.perf_counter()
+    download_time = end_download - start_download
+    
+    multipart_data = MultipartEncoder(
+        fields={
+            "file": (
+                file_name,
+                binary_data,
+                "application/octet-stream",
             )
+        }
+    )
+
+
+    upload_url = _get_upload_url(binary_record)
+
+    start_upload = time.perf_counter()
+    upload_response = requests.post(
+        upload_url,
+        data=multipart_data,
+        headers={
+            "Authtoken": context.get_auth_token(),
+            "Content-Type": multipart_data.content_type,
+        },
+    )
+    end_upload = time.perf_counter()
+    upload_time = end_upload - start_upload
+        
+    if upload_response.status_code != 200:
+        context.log(f"[PID {pid}] Upload failed: {upload_response.text}")
+        raise UploadError(
+            f"Failed to upload binary file '{file_name}': {upload_response.status_code} - {upload_response.text}"
+        )
         
 
     end_migrate = time.perf_counter()
     migrate_time = end_migrate - start_migrate
-    context.log(f"[PID {pid}] 🥳 Migrated binary file '{file_name}' from Fedora in {migrate_time:.2f} seconds")
+    context.log(f"[PID {pid}] 🥳 Migrated binary file '{file_name}' from Fedora in {migrate_time:.2f}s (⬇️{download_time:.2f}s, ⬆️{upload_time:.2f}s) ")
 
 
 
